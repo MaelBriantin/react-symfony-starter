3d0a67d3e2e2bae3ae9dd46187e2a4a340f08b66
MaelBriantin <mael.briantin@proton.me>
2025-04-18 16:35:16 +0200

    feat(client): add  Tailwind CSS support

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from "react";
+import "./App.css";
+
+type ItemType = {
+  name: string,
+  url: string,
+  src: string
+};
+
+const items: ItemType[] = [
+  { name: "Vite", url: "https://vite.dev", src: "/vite.svg" },
+  { name: "React", url: "https://react.dev", src: "/react.svg"},
+];
+
+const itemCard = (item: ItemType) => {
+  const hoverColor = item.name === "Vite" ? "hover:text-vite" : "hover:text-react";
+  const shadowColor = item.name === "Vite" ? "hover:drop-shadow-[0_0_2em_var(--color-vite)]" :  "hover:drop-shadow-[0_0_2em_var(--color-react)]";
+  
+  return (
+    <div className="flex justify-center flex-col gap-2" key={item.name}>
+      <a href={item.url} target="_blank">
+        <img
+          src={item.src}
+          alt={`${item.name} logo`}
+          className={[
+            "h-[10em]",
+            "p-[1.5em]",
+            `${shadowColor}`,
+          ].join(" ")}
+        />
+      </a>
+      <a
+        href={item.url}
+        target="_blank"
+        className={`text-4xl font-bold text-center transition-colors ${hoverColor}`}
+      >
+        {item.name}
+      </a>
+    </div>
+  );
+}
+      
+  
+
+const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className="flex flex-col w-screen h-screen items-center justify-center gap-6 dark:bg-neutral-800 dark:text-gray-100 text-black bg-neutral-100">
+        <div className="flex justify-center gap-8">
+          {items.map(item => 
+            itemCard(item)
+          )}
+        </div>
+        <div className="px-4 py-2 flex flex-col items-center justify-center gap-4">
+          <button
+            type="button"
+            onClick={() => setCount((count: number) => count + 1)}
+          >
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="text-gray-400">
+          Click on the Vite, React and Deno logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
