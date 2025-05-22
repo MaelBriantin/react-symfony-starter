3897319b265c8d821e057601eada313400b0a944
MaelBriantin <mael.briantin@proton.me>
2025-05-22 16:02:49 +0200

    test(arch): add architectural tests for strict type usage and enum validation

--- app/api/tests/Arch/ArchTest.php
+++ app/api/tests/Arch/ArchTest.php
@@ -4,3 +4,20 @@ arch()
     ->expect('App')
     ->toUseStrictTypes()
     ->not->toUse(['die', 'dd', 'dump']);
+
+arch()
+    ->expect('App\Domain\Data\Enum')
+    ->toBeEnums();
+
+arch()
+    ->expect('App\Domain\**')
+    ->not->toUse('App\Infrastructure\**');
+
+arch()
+    ->expect('App\Infrastructure\**')
+    ->not->toUse('App\Application\**');
+
+arch()
+    ->expect('App\Application\**')
+    ->not->toUse('App\Infrastructure\**');
+
